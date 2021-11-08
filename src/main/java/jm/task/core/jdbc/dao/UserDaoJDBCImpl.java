98a6a35d8bc85a006bd31cfa148ee62a041242e6
mitichkinivan <mitichkin.ivan@gmail.com>
2021-11-08 21:00:47 +0700

    6

--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -63,19 +63,17 @@ public class UserDaoJDBCImpl implements UserDao {
         List<User> arr = new ArrayList<>();
 
         try (Statement statement = Util.getConnection().createStatement()) {
-            try (ResultSet resultSet = statement.executeQuery("SELECT * FROM users")) {
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
 
-                while (resultSet.next()) {
-                    User user = new User();
-                    user.setId(resultSet.getLong("id"));
-                    user.setName(resultSet.getString("name"));
-                    user.setLastName(resultSet.getString("lastName"));
-                    user.setAge(resultSet.getByte("age"));
-                    arr.add(user);
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setLastName(resultSet.getString("lastName"));
+                user.setAge(resultSet.getByte("age"));
+                arr.add(user);
             }
+            resultSet.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
